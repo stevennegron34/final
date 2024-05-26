57cd19077276168cab5a1db4d69db762c147c097
Steven <156080251+stevennegron97@users.noreply.github.com>
2024-05-26 19:03:14 +0600

    new

---
+++ index.php
@@ -0,0 +1,68 @@
+
+
+<!doctype html>
+<html lang="en">
+  
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="language" content="English">
+        <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1" />
+        <meta name="viewport" content="viewport-fit=cover,width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0">
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Autoembed ─ Watch latest movies online">
+        <meta property="og:description" content="autoembed.cc is official site of autoembed. Watch latest Hollywood, Bollywood, korean movies, Tv Shows, Dual Audio Movies and Web Series, Netflix WEB Series, Amazon Prime Series and lots more Series in Dual Audio (English and Hindi)">
+        <meta property="og:url" content="https://watch.autoembed.cc">
+        <meta property="og:image" content="https://watch.autoembed.cc/banner.webp">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        
+        <link rel="manifest" href="manifest.json">
+        <link href="http://fonts.googleapis.com/css?family=Nanum+Gothic:400,700,800" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700,800" rel="stylesheet">
+       
+        <title>Autoembed ─ Watch latest movies online</title>
+        <meta name="description" content="autoembed.cc is official site of moviex4u. Watch latest Hollywood, Bollywood, korean movies, Tv Shows, Dual Audio Movies and Web Series, Netflix WEB Series, Amazon Prime Series and lots more Series in Dual Audio (English and Hindi)"/>
+        <meta name="keywords" content="autoembed.cc, autoembed, watch movies online, watch tv shows online, Free movies 2024, watch free movies in hd quality"/>
+        <link rel="icon" type="image/x-icon" href="/favicon.webp">
+        <link rel="canonical" href="https://watch.autoembed.cc"/>
+        <link rel="sitemap" type="application/xml" title="Sitemap" href="https://watch.autoembed.cc/sitemap.xml">
+        <script type="application/ld+json">
+          {
+            "@context": "http://schema.org",
+            "@type": "Movie",
+            "name": "Autoembed ─ Watch latest movies online",
+            "description": "autoembed.cc is official site of autoembed.cc. Watch latest Hollywood, Bollywood, korean movies, Tv Shows, Dual Audio Movies and Web Series, Netflix WEB Series, Amazon Prime Series and lots more Series in Dual Audio (English and Hindi)",
+            "url": "https://watch.autoembed.cc,
+            "image": "https://watch.autoembed.cc/banner.webp"
+           }
+        </script>
+        <script defer="defer" src="/commons-main-index.js.bundle.js"></script>
+        <script defer="defer" src="/commons-main-index.esm2017.js.bundle.js"></script>
+        <script defer="defer" src="/commons-main-vtt.js.bundle.js"></script>
+        <script defer="defer" src="/commons-main-video.es.js.bundle.js"></script>
+        <script defer="defer" src="/commons-main-react.development.js.bundle.js"></script>
+        <script defer="defer" src="/commons-main-react-toastify.esm.js.bundle.js"></script>
+        <script defer="defer" src="/commons-main-react-responsive.js.bundle.js"></script>
+        <script defer="defer" src="/commons-main-Helmet.js.bundle.js"></script>
+        <script disable-devtool-auto src="https://fastly.jsdelivr.net/npm/disable-devtool/disable-devtool.min.js"></script>
+        <script defer="defer" src="/commons-main-react-dom.development.js.bundle.js"></script>
+        <script defer="defer" src="/commons-main-factoryWithTypeCheckers.js.bundle.js"></script>
+        <script defer="defer" src="/commons-main-mpd-parser.es.js.bundle.js"></script>
+        <script defer="defer" src="/commons-main-moment.min.js.bundle.js"></script>
+        <script defer="defer" src="/commons-main-m3u8-parser.es.js.bundle.js"></script>
+        <script defer="defer" src="/commons-main-history.js.bundle.js"></script>
+        <script defer="defer" src="/commons-main-dom.js.bundle.js"></script>
+        <script defer="defer" src="/commons-main-internal.js.bundle.js"></script>
+        <script defer="defer" src="/commons-main-index-680e2b70.js.bundle.js"></script>
+        <script defer="defer" src="/main.bundle.js"></script>
+        <link href="/Assets/CSS/main.bundle.css" rel="stylesheet">
+    </head>
+    <body>
+        <div id="root"></div>
+        
+    </body>
+    
+</html>
